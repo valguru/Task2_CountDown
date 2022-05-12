1a7d3fff99089690ea8f2e865a594f76a736eae1
valguru <gurina.va@edu.spbstu.ru>
2022-05-13 01:42:26 +0300

    Добавьте файлы проекта.

---
+++ Task2/CountDown.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Task2
+{
+    public class CountDown
+    {
+        public delegate void EventCallback(string message);
+
+        Dictionary<string, EventCallback> events;
+        public CountDown()
+        {
+            events = new Dictionary<string, EventCallback>();
+        }
+        public void AddEvent(string name, EventCallback callback)
+        {
+            events.Add(name, callback);
+        }
+
+        public void RemoveEvent(string name)
+        {
+            events.Remove(name);
+        }
+
+        public void Skip(int ms, string message)
+        {
+            Thread.Sleep(ms);
+            foreach (var eventCallback in events)
+            {
+                eventCallback.Value(message);
+            }
+        }
+    }
+
+    public class Subscriber
+    {
+        public Subscriber(CountDown cd, string name)
+        {
+            SubscribeName = name;
+            CdObject = cd;
+            cd.AddEvent(name, (message) => Display(message));
+        }
+
+        public void Ubsubscribe()
+        {
+            CdObject.RemoveEvent(SubscribeName);
+        }
+
+        private void Display(string eventText)
+        {
+            Console.WriteLine("Event: {0} \n Subscriber: {1}", eventText, SubscribeName);
+        }
+        private CountDown CdObject;
+        private string SubscribeName;
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer = new CountDown();
+
+            var subscriberA = new Subscriber(timer, "Андрей");
+            var subscriberB = new Subscriber(timer, "Борис");
+            var subscriberC = new Subscriber(timer, "Василий");
+
+            timer.Skip(1000, "Событие 1");
+            timer.Skip(1000, "Событие 2");
+            subscriberA.Ubsubscribe();
+            timer.Skip(2000, "Событие 3");
+            subscriberB.Ubsubscribe();
+            timer.Skip(1000, "Событие 4");
+            subscriberC.Ubsubscribe();
+            timer.Skip(1000, "Событие 5");
+            Console.ReadKey();
+        }
+    }
+}
